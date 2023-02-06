0159cb0362fba7ad09c017fc072b96ff110b9b5f
restuprayudha797 <yudariau1@gmail.com>
2023-02-06 07:47:27 +0700

    added Controllee

---
+++ application/controllers/Auth.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    public function index()
+    {
+        // add views
+    }
+
+
+    public function Register()
+    {
+        // add views
+    }
+}
