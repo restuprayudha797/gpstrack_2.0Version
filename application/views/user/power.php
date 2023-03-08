7a79b6698d79fd04553c3f3fdda3a6f6a49c2536
restuprayudha797 <yudariau1@gmail.com>
2023-03-08 08:49:48 +0700

    test

--- application/views/user/power.php
+++ application/views/user/power.php
@@ -30,79 +30,79 @@ foreach ($states as $state) {
 ?>
 <!-- ========== section start ========== -->
 <section class="section">
-  <div class="container-fluid">
-    <!-- ========== title-wrapper start ========== -->
-    <div class="title-wrapper pt-30">
-      <div class="row align-items-center">
-        <div class="col-md-6">
-          <div class="title mb-30">
-            <h2><?= $title; ?></h2>
-          </div>
-        </div>
-        <!-- end col -->
-        <div class="col-md-6">
-          <div class="breadcrumb-wrapper mb-30">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                  <a href="#0">Dashboard</a>
-                </li>
-                <li class="breadcrumb-item active" aria-current="page">
-                  Power
-                </li>
-              </ol>
-            </nav>
-          </div>
-        </div>
-        <!-- end col -->
-      </div>
-      <!-- end row -->
-
-      <!-- Content -->
-
-      <?php if ($waktuSekarang >= $active_check['time_out']) : ?>
-
-        <h3> Paket anda sudah berakhir silahkan melakukan check out kembali</h3>
-
-      <?php else : ?>
-        <div class="container">
-
-          <div class="container p-5 border text-center">
-
-            <div class="titile">
-              <h6></h6>Klick tombol dibawah ini untuk mengidupkan atau mematikan motor</h6>
+    <div class="container-fluid">
+        <!-- ========== title-wrapper start ========== -->
+        <div class="title-wrapper pt-30">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="title mb-30">
+                        <h2><?= $title; ?></h2>
+                    </div>
+                </div>
+                <!-- end col -->
+                <div class="col-md-6">
+                    <div class="breadcrumb-wrapper mb-30">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <a href="#0">Dashboard</a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    Power
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+                <!-- end col -->
             </div>
-            <?php if ($colorState == 1) : ?>
-              <div class="gambar mt-3">
-                <a href="<?= base_url('user/off') ?>">
-                  <img src="<?= base_url('assets/images/power/on.png') ?>" width="100px" alt="">
-                </a>
-                <br>
-                <b class="mt-3">Hidup</b>
-              </div>
+            <!-- end row -->
+
+            <!-- Content -->
+
+            <?php if ($waktuSekarang >= $active_check['time_out']) : ?>
+
+            <h3> Paket anda sudah berakhir silahkan melakukan check out kembali</h3>
 
             <?php else : ?>
+            <div class="container">
 
-              <div class="gambar mt-3">
-                <a href="<?= base_url('user/on') ?>">
-                  <img src="<?= base_url('assets/images/power/off.png') ?>" width="100px" alt="">
-                </a>
-                <br>
-                <b class="mt-3">Mati</b>
-              </div>
+                <div class="container p-5 border text-center">
 
-            <?php endif ?>
+                    <div class="titile">
+                        <h6></h6>Klick tombol dibawah ini untuk mengidupkan atau mematikan motor</h6>
+                    </div>
+                    <?php if ($colorState == 1) : ?>
+                    <div class="gambar mt-3">
+                        <a href="<?= base_url('user/off') ?>">
+                            <img src="<?= base_url('assets/images/power/on.png') ?>" width="100px" alt="">
+                        </a>
+                        <br>
+                        <b class="mt-3">Hidup</b>
+                    </div>
+
+                    <?php else : ?>
+
+                    <div class="gambar mt-3">
+                        <a href="<?= base_url('user/on') ?>">
+                            <img src="<?= base_url('assets/images/power/off.png') ?>" width="100px" alt="">
+                        </a>
+                        <br>
+                        <b class="mt-3">Mati</b>
+                    </div>
+
+                    <?php endif ?>
 
 
-          </div>
+                </div>
 
 
 
-        <?php endif; ?>
-        <!-- End Content -->
+                <?php endif; ?>
+                <!-- End Content -->
 
-        <!-- ========== title-wrapper end ========== -->
-        </div>
-        <!-- end container -->
+                <!-- ========== title-wrapper end ========== -->
+            </div>
+            <!-- end container -->
 </section>
 <!-- ========== section end ========== -->
